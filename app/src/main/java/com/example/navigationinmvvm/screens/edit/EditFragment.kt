d6c553e3a7474a887da627e25e9c44624bedb333
ArsenKoch <arsenkocaran@icloud.com>
2023-02-05 19:42:02 +0300

    Create a new class EditFragment.kt

---
+++ app/src/main/java/com/example/navigationinmvvm/screens/edit/EditFragment.kt
@@ -0,0 +1,10 @@
+package com.example.navigationinmvvm.screens.edit
+
+import com.example.navigationinmvvm.screens.base.BaseScreen
+
+class EditFragment {
+
+    class Screen(
+        val initialValue: String
+    ) : BaseScreen
+}
