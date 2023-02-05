74226b2b855618a296807a0603a42d1bf93f5cc6
ArsenKoch <arsenkocaran@icloud.com>
2023-02-05 19:24:50 +0300

    Add a new methods in Navigator.kt interface

--- app/src/main/java/com/example/navigationinmvvm/navigator/Navigator.kt
+++ app/src/main/java/com/example/navigationinmvvm/navigator/Navigator.kt
@@ -1,4 +1,14 @@
 package com.example.navigationinmvvm.navigator
 
+import androidx.annotation.StringRes
+
 interface Navigator {
+
+    fun launch()
+
+    fun goBack(result: Any? = null)
+
+    fun toast(@StringRes messageRes: Int)
+
+    fun getString(@StringRes messageRes: Int): String
 }
