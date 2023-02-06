a448a88536427cbdccb68b6810b36e59b8d838d1
ArsenKoch <arsenkocaran@icloud.com>
2023-02-06 22:51:53 +0300

    Realize a MainNavigator.kt class. Part 1

--- app/src/main/java/com/example/navigationinmvvm/navigator/MainNavigator.kt
+++ app/src/main/java/com/example/navigationinmvvm/navigator/MainNavigator.kt
@@ -1,4 +1,27 @@
 package com.example.navigationinmvvm.navigator
 
-class MainNavigator {
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import com.example.navigationinmvvm.screens.base.BaseScreen
+
+class MainNavigator(
+    application: Application
+) : AndroidViewModel(application), Navigator {
+
+    override fun launch(screen: BaseScreen) {
+        TODO("Not yet implemented")
+    }
+
+    override fun goBack(result: Any?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun toast(messageRes: Int) {
+        Toast.makeText(getApplication(), messageRes, Toast.LENGTH_LONG).show()
+    }
+
+    override fun getString(messageRes: Int): String {
+        return getApplication<Application>().getString(messageRes)
+    }
 }
