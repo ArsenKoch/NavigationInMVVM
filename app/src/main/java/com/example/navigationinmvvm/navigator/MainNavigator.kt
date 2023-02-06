693944cd3b41e4c1f35fe64e5b0689bb5f416e9f
ArsenKoch <arsenkocaran@icloud.com>
2023-02-06 23:22:23 +0300

    Realize a class MainNavigator.kt. Part 2

--- app/src/main/java/com/example/navigationinmvvm/navigator/MainNavigator.kt
+++ app/src/main/java/com/example/navigationinmvvm/navigator/MainNavigator.kt
@@ -2,19 +2,37 @@ package com.example.navigationinmvvm.navigator
 
 import android.app.Application
 import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.navigationinmvvm.Event
+import com.example.navigationinmvvm.MainActivity
+import com.example.navigationinmvvm.R
 import com.example.navigationinmvvm.screens.base.BaseScreen
 
+const val ARG_SCREEN = "SCREEN"
+
+@Suppress("DEPRECATION")
 class MainNavigator(
     application: Application
 ) : AndroidViewModel(application), Navigator {
 
-    override fun launch(screen: BaseScreen) {
-        TODO("Not yet implemented")
+    val whenActivityActive = MainActivityActions()
+
+    private val _result = MutableLiveData<Event<Any>>()
+    val result: LiveData<Event<Any>> = _result
+
+    override fun launch(screen: BaseScreen) = whenActivityActive {
+        launchFragment(it, screen)
     }
 
-    override fun goBack(result: Any?) {
-        TODO("Not yet implemented")
+    override fun goBack(result: Any?) = whenActivityActive {
+        if (result != null) {
+            _result.value = Event(result)
+        }
+        it.onBackPressed()
     }
 
     override fun toast(messageRes: Int) {
@@ -23,5 +41,20 @@ class MainNavigator(
 
     override fun getString(messageRes: Int): String {
         return getApplication<Application>().getString(messageRes)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        whenActivityActive.clear()
+    }
+
+    fun launchFragment(activity: MainActivity, screen: BaseScreen, addToBackStack: Boolean = true) {
+        val fragment = screen.javaClass.enclosingClass.newInstance() as Fragment
+        fragment.arguments = bundleOf(ARG_SCREEN to screen)
+        val transaction = activity.supportFragmentManager.beginTransaction()
+        if (addToBackStack) transaction.addToBackStack(null)
+        transaction
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
