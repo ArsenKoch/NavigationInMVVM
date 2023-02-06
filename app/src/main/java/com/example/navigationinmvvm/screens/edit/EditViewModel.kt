0caa9c79bbfbe0e6f937ca581bef255b230953ee
ArsenKoch <arsenkocaran@icloud.com>
2023-02-06 22:22:19 +0300

    Realize a class EditViewModel.kt

--- app/src/main/java/com/example/navigationinmvvm/screens/edit/EditViewModel.kt
+++ app/src/main/java/com/example/navigationinmvvm/screens/edit/EditViewModel.kt
@@ -1,4 +1,33 @@
 package com.example.navigationinmvvm.screens.edit
 
-class EditViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.navigationinmvvm.Event
+import com.example.navigationinmvvm.R
+import com.example.navigationinmvvm.navigator.Navigator
+import com.example.navigationinmvvm.screens.base.BaseViewModel
+
+class EditViewModel(
+    private val navigator: Navigator,
+    screen: EditFragment.Screen
+) : BaseViewModel() {
+
+    private val _initialMessageEvent = MutableLiveData<Event<String>>()
+    val initialMessageEvent: LiveData<Event<String>> = _initialMessageEvent
+
+    init {
+        _initialMessageEvent.value = Event(screen.initialValue)
+    }
+
+    fun onSavePressed(message: String) {
+        if (message.isBlank()) {
+            navigator.toast(R.string.empty_message)
+            return
+        }
+        navigator.goBack(message)
+    }
+
+    fun onCancelPressed() {
+        navigator.goBack()
+    }
 }
