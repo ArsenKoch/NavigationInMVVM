57315d710f3068ddad1303a015de73ef6a837f15
ArsenKoch <arsenkocaran@icloud.com>
2023-02-06 22:29:36 +0300

    Fragment realize for the start screen

--- app/src/main/java/com/example/navigationinmvvm/screens/hello/HelloFragment.kt
+++ app/src/main/java/com/example/navigationinmvvm/screens/hello/HelloFragment.kt
@@ -1,9 +1,34 @@
 package com.example.navigationinmvvm.screens.hello
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.navigationinmvvm.databinding.FragmentHelloBinding
+import com.example.navigationinmvvm.screens.base.BaseFragment
 import com.example.navigationinmvvm.screens.base.BaseScreen
+import com.example.navigationinmvvm.screens.base.BaseViewModel
+import com.example.navigationinmvvm.screens.base.screenViewModel
 
-class HelloFragment {
-
+class HelloFragment : BaseFragment() {
 
     class Screen : BaseScreen
+
+    override val viewModel by screenViewModel<HelloViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentHelloBinding.inflate(inflater, container, false)
+
+        binding.btnEdit.setOnClickListener { viewModel.onEditPressed() }
+
+        viewModel.currentMessage.observe(viewLifecycleOwner) {
+            binding.tvValue.text = it
+        }
+
+        return binding.root
+    }
 }
