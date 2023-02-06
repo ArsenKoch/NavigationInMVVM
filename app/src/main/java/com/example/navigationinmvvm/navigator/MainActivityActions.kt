b160af6b2947330bd02580a0213083500c8c999b
ArsenKoch <arsenkocaran@icloud.com>
2023-02-06 23:00:04 +0300

    Realize a class MainActivityActions.kt

--- app/src/main/java/com/example/navigationinmvvm/navigator/MainActivityActions.kt
+++ app/src/main/java/com/example/navigationinmvvm/navigator/MainActivityActions.kt
@@ -1,4 +1,32 @@
 package com.example.navigationinmvvm.navigator
 
+import com.example.navigationinmvvm.MainActivity
+
+typealias MainActivityAction = (MainActivity) -> Unit
+
 class MainActivityActions {
+
+    var mainActivity: MainActivity? = null
+        set(activity) {
+            field = activity
+            if (activity != null) {
+                actions.forEach { it(activity) }
+                clear()
+            }
+        }
+
+    private val actions = mutableListOf<MainActivityAction>()
+
+    operator fun invoke(action: MainActivityAction) {
+        val activity = this.mainActivity
+        if (activity == null) {
+            actions += action
+        } else {
+            action(activity)
+        }
+    }
+
+    fun clear() {
+        actions.clear()
+    }
 }
